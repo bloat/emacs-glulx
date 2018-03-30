2e73361f565ca24778664b3c1c01c5d03d5a4371
Andrew Cowper <andrew.cowper@slothrop.net>
2018-03-30 16:48:32 +0100

    Removed C code.
    No longer interested in running glk in emacs with an external
    interpreter.

--- glk/src/glk-c-lib/elisp_glk_encoding.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "glk.h"
-#include "elisp_glk.h"
-
-unsigned char glk_char_to_lower(unsigned char ch)
-{
-  printf("(glk-char-to-lower (glki-char \"%c\"))\n", ch);
-  return get_next_char();
-}
--- glk/src/glk-c-lib/glk-structure.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "glk.h"
-
-void glk_exit(void)
-{
-  printf("(glk-exit)\n");
-}
